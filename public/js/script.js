de2809432a65a2700ec5d76507f4016765b9e384
Alfonso Robles <fons3517@gmail.com>
2021-11-08 20:23:53 -0600

    edited files to restore session and correct server.js file issues

--- public/js/script.js
+++ public/js/script.js
@@ -1,7 +1,7 @@
 // Client-side socket script
 import { io } from 'socket.io-client';
-const messageInput = document.getElementById();
-const form = document.getElementById();
+const messageInput = document.getElementById('message-input');
+const form = document.getElementById('form');
 
 const socket = io(
   'http://localhost:3001' || 'https://obscure-scrubland-68562.herokuapp.com/'
